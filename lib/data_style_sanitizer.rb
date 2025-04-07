1547d08eb77e92f468d0d3cabee0635d61b64a88
tedaford <ted.ford@fortra.com>
2025-04-07 17:27:59 -0400

    Initial Commit

---
+++ lib/data_style_sanitizer.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "data_style_sanitizer/version"
+
+module DataStyleSanitizer
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/data_style_sanitizer/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module DataStyleSanitizer
+  VERSION = "0.1.0"
+end
